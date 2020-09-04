4dc686ee19584415e83e7468c44a435f89c74f83
Rodrigo Sampaio <digo.15rj@gmail.com>
2020-09-04 16:22:14 -0300

    Validacao sintatica com bean validation

--- src/main/java/com/rodrigosampaio/cursomc/services/CategoriaService.java
+++ src/main/java/com/rodrigosampaio/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rodrigosampaio.cursomc.domain.Categoria;
+import com.rodrigosampaio.cursomc.dto.CategoriaDTO;
 import com.rodrigosampaio.cursomc.repositories.CategoriaRepository;
 import com.rodrigosampaio.cursomc.services.exceptions.DataIntegrityException;
 import com.rodrigosampaio.cursomc.services.exceptions.ObjectNotFoundException;
@@ -61,5 +62,9 @@ public class CategoriaService {
 		PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		
 		return categoriaRepository.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO (CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
